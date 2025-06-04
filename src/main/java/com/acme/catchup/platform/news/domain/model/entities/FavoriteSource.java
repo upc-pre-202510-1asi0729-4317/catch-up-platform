e97e2603db9bbaf3f47a2bd3548d017526b64ed2
alberto sanchez <albertosanchez05@gmail.com>
2025-06-04 18:57:57 -0500

    fix: add entity listener for entity

--- src/main/java/com/acme/catchup/platform/news/domain/model/entities/FavoriteSource.java
+++ src/main/java/com/acme/catchup/platform/news/domain/model/entities/FavoriteSource.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class FavoriteSource {
 
     @Id
